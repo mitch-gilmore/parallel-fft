7166510dbbf0261d4375c00933a5b463a225d563
Austin Heath <agheath@scc1.bu.edu>
2024-05-02 19:11:28 -0400

    prep

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # parallel-fft
+
+## Running basic FFT functions & timing
+
+Code exists in `./fft_sparse` directory.
+
+Run the following to execute:
+
+```
+cd ./fft_sparse && make && ./fft_sparse.out
+```
+
+See existing research on sparse FFTs in the `./papers` directory.
--- fft_sparse/fft_sparse.cpp
+++ fft_sparse/fft_sparse.cpp
@@ -32,52 +32,49 @@ void find_largest(int size, Complex * out) {
     int lg_index = 0;
 
     for (int i = 0; i < size; i++) {
-        magnitude = sqrt(out[i].real()*out[i].real()+out[i].imag()*out[i].imag());
-        if (i == 0 || magnitude > largest) {
+        magnitude = sqrt(pow(out[i].real(),2)+pow(out[i].imag(),2));
+        if (magnitude > largest) {
             largest = magnitude;
             lg_index = i;
         }
     }
 
-    printf("Found magnitude %.8f at index %d\n\n", magnitude, lg_index);
+    printf("Found magnitude %.8f at index %d\n\n", largest, lg_index);
 }
 
 int main(void) {
+    double time;
+
     Complex * in = new Complex[SIZE];
     Complex * out = new Complex[SIZE];
 
     Complex * in_cp = new Complex[SIZE];
     Complex * out_cp = new Complex[SIZE];
 
+    printf("\nInitializing 3D test data...\n");
+
     generate_data(NX, NY, NZ, in);
-
-    double time;
-
     copy(SIZE, in, in_cp);
-    copy(SIZE, out, out_cp);
-    
-    printf("\nInitializing...\n");
 
     time = dft_3d_naive(NX, NY, NZ, in_cp, out_cp);
     printf("[DFT 3D NAIVE] time ms: %.8f \n", time);
 
     copy(SIZE, in, in_cp);
-    copy(SIZE, out, out_cp);
 
     time = dft_3d_fft(NX, NY, NZ, in_cp, out_cp);
-    printf("[DFT 3D CUSTOM FFT] time ms: %.8f \n", time);
+    printf("[FFT 3D NAIVE] time ms: %.8f \n", time);
 
     copy(SIZE, in, in_cp);
-    copy(SIZE, out, out_cp);
 
     time = dft_3d_fft_threads(NX, NY, NZ, in_cp, out_cp);
-    printf("[DFT 3D CUSTOM FFT THREADS] time ms: %.8f \n", time);
+    printf("[FFT 3D THREADS] time ms: %.8f \n", time);
 
     copy(SIZE, in, in_cp);
-    copy(SIZE, out, out_cp);
 
     time = dft_3d_fftw(NX, NY, NZ, in_cp, out_cp);
-    printf("[DFT 3D FFTW] time ms: %.8f \n", time);
+    printf("[FFT 3D FFTW] time ms: %.8f \n", time);
+
+    printf("[FFT 3D SPARSE] time ms: IN PROGRESS (see fft_sparse.cpp and references)\n");
 
     printf("Finished...\n");
 
